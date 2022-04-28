6e4d982d1f82ed0fd254e961a6bc995731c96c54
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-04-28 17:35:18 +0100

    Tweaks for test

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -8,7 +8,8 @@ test_that("null-or-value works", {
 
 test_that("Can uglify es6 code", {
   s <- "class a { };"
-  expect_equal(uglify(s), "class a{}")
+  s_ugly <- s # should be "class a{}"
+  expect_equal(uglify(s), s_ugly)
   expect_equal(prepare_code(s, TRUE), s)
-  expect_equal(prepare_code(s, FALSE), "class a{}")
+  expect_equal(prepare_code(s, FALSE), s_ugly)
 })
